2e912b554891b0ed71356c9da54d6cbe8f264397
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2023-01-09 18:24:27 -0800

    feedback

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ locals {
   private_cidr_blocks = slice(local.subnet_cidr_blocks, local.num_of_azs, length(local.subnet_cidr_blocks))
   ## Determine cidr blocks for pod network
   base_pod_cidr_network_bits = tonumber(regex("[^/]*$", var.pod_cidr))
-  pod_cidr_blocks = var.use_pod_cidr == false ? [] : cidrsubnets(
+  pod_cidr_blocks = !var.use_pod_cidr ? [] : cidrsubnets(
     var.pod_cidr,
     [for n in range(0, local.num_of_azs) : var.pod_cidr_network_bits - local.base_pod_cidr_network_bits]...
   )
