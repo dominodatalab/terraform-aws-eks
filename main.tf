fef2a43c440700e7f475a96a1097a5a61f61f037
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2023-02-08 17:03:34 -0800

    also set instance tags from tags

--- main.tf
+++ main.tf
@@ -159,7 +159,7 @@ locals {
     merge(var.additional_node_groups, var.default_node_groups) :
     name => merge(ng, {
       gpu           = ng.gpu != null ? ng.gpu : anytrue([for itype in ng.instance_types : length(data.aws_ec2_instance_type.all[itype].gpus) > 0]),
-      instance_tags = merge(data.aws_default_tags.this.tags, ng.tags)
+      instance_tags = merge(data.aws_default_tags.this.tags, var.tags, ng.tags)
     })
   }
 }
