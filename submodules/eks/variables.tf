c060388c0b2c677255a312bec30405f61087d97e
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2023-02-13 19:30:50 -0800

    allow selection of specific AZs in node pools

--- submodules/eks/variables.tf
+++ submodules/eks/variables.tf
@@ -35,6 +35,7 @@ variable "node_groups" {
     max_per_az           = number
     desired_per_az       = number
     number_of_azs        = optional(number, null)
+    availability_zones   = optional(list(string), null)
     labels               = map(string)
     taints               = optional(list(object({ key = string, value = optional(string), effect = string })), [])
     tags                 = optional(map(string), {})
