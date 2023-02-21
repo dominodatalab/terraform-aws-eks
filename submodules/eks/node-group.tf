2dc801ec8a0eb889da158aa1312ba8ea18c6f60d
Miguel Harmant <miguel.harmant@dominodatalab.com>
2023-02-21 11:52:17 -0500

    Add force-upgrade param

--- submodules/eks/node-group.tf
+++ submodules/eks/node-group.tf
@@ -144,14 +144,15 @@ data "aws_ssm_parameter" "eks_gpu_ami_release_version" {
 }
 
 resource "aws_eks_node_group" "node_groups" {
-  depends_on      = [module.k8s_setup]
-  for_each        = local.node_groups_by_name
-  cluster_name    = aws_eks_cluster.this.name
-  version         = aws_eks_cluster.this.version
-  release_version = each.value.node_group.ami != null ? null : (each.value.node_group.gpu ? nonsensitive(data.aws_ssm_parameter.eks_gpu_ami_release_version.value) : nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value))
-  node_group_name = "${local.eks_cluster_name}-${each.key}"
-  node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = [each.value.subnet.subnet_id]
+  depends_on           = [module.k8s_setup]
+  for_each             = local.node_groups_by_name
+  cluster_name         = aws_eks_cluster.this.name
+  version              = aws_eks_cluster.this.version
+  release_version      = each.value.node_group.ami != null ? null : (each.value.node_group.gpu ? nonsensitive(data.aws_ssm_parameter.eks_gpu_ami_release_version.value) : nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value))
+  node_group_name      = "${local.eks_cluster_name}-${each.key}"
+  node_role_arn        = aws_iam_role.eks_nodes.arn
+  subnet_ids           = [each.value.subnet.subnet_id]
+  force_update_version = true
   scaling_config {
     min_size     = each.value.node_group.min_per_az
     max_size     = each.value.node_group.max_per_az
