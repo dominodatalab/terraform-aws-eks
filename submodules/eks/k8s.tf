7c69de91435903d27810f322f99b21157a23f7b8
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2023-04-21 11:12:42 -0700

    Switch to terraform_data

--- submodules/eks/k8s.tf
+++ submodules/eks/k8s.tf
@@ -14,12 +14,12 @@ module "k8s_setup" {
   depends_on = [aws_eks_addon.vpc_cni, null_resource.kubeconfig]
 }
 
-resource "null_resource" "run_k8s_pre_setup" {
+resource "terraform_data" "run_k8s_pre_setup" {
   count = local.run_setup
 
-  triggers = {
-    change_hash = module.k8s_setup[0].change_hash
-  }
+  triggers_replace = [
+    module.k8s_setup[0].change_hash
+  ]
 
   provisioner "local-exec" {
     command     = "./${module.k8s_setup[0].filename} set_k8s_auth set_eniconfig"
@@ -30,12 +30,12 @@ resource "null_resource" "run_k8s_pre_setup" {
   depends_on = [module.k8s_setup]
 }
 
-resource "null_resource" "calico_setup" {
+resource "terraform_data" "calico_setup" {
   count = local.run_setup
 
-  triggers = {
-    change_hash = module.k8s_setup[0].change_hash
-  }
+  triggers_replace = [
+    module.k8s_setup[0].change_hash
+  ]
 
   provisioner "local-exec" {
     command     = "./${module.k8s_setup[0].filename} install_calico"
@@ -43,5 +43,5 @@ resource "null_resource" "calico_setup" {
     working_dir = module.k8s_setup[0].resources_directory
   }
 
-  depends_on = [aws_eks_node_group.node_groups, null_resource.run_k8s_pre_setup]
+  depends_on = [aws_eks_node_group.node_groups, terraform_data.run_k8s_pre_setup]
 }
