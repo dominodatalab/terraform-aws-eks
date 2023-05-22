b722596f642a552f96d22308ca5f25fee3bfe626
Miguel Harmant <miguel.harmant@dominodatalab.com>
2023-05-22 15:32:35 -0400

    Revise eks perms

--- iam.tf
+++ iam.tf
@@ -38,22 +38,31 @@ resource "aws_iam_role_policy_attachment" "route53" {
   role       = lookup(module.eks.info.nodes.roles[count.index], "name")
 }
 
+
+locals {
+  create_eks_role_name = "${var.deploy_id}-create-eks"
+}
+
 data "aws_iam_policy_document" "create_eks_role" {
   statement {
     sid = "EKSDeployerEKS"
     actions = [
-      "eks:*Addon*",
-      "eks:CreateCluster",
-      "eks:DeleteCluster",
-      "eks:CreateNodegroup",
-      "eks:DescribeCluster",
-      "eks:DescribeNodegroup",
-      "eks:DescribeUpdate",
-      "eks:ListTagsForResource",
-      "eks:TagResource",
-      "eks:UntagResource"
+      "eks:*"
     ]
-    resources = ["*"]
+    resources = [
+      "arn:${data.aws_partition.current.partition}:eks:${var.region}:${local.aws_account_id}:cluster/${var.deploy_id}",
+      "arn:${data.aws_partition.current.partition}:eks:${var.region}:${local.aws_account_id}:cluster/${var.deploy_id}/*"
+    ]
+    effect = "Allow"
+  }
+
+  statement {
+    sid = "EKSDeployerKMS"
+    actions = [
+      "kms:CreateGrant",
+      "kms:DescribeKey"
+    ]
+    resources = ["arn:${data.aws_partition.current.partition}:kms:${var.region}:${local.aws_account_id}:key/*"]
     effect    = "Allow"
   }
 
@@ -63,29 +72,19 @@ data "aws_iam_policy_document" "create_eks_role" {
       "iam:PassRole",
       "iam:GetRole"
     ]
-    resources = ["arn:aws:iam::${local.aws_account_id}:role/${var.deploy_id}*"]
-    effect    = "Allow"
-  }
-
-  statement {
-    sid = "EKSDeployerKMS"
-    actions = [
-      "kms:CreateGrant",
-      "kms:DescribeKey"
-    ]
-    resources = ["*"]
+    resources = ["arn:${data.aws_partition.current.partition}:iam::${local.aws_account_id}:role/${var.deploy_id}-*"]
     effect    = "Allow"
   }
 }
 
 resource "aws_iam_policy" "create_eks_role" {
-  name   = "${var.deploy_id}-create-eks"
+  name   = local.create_eks_role_name
   path   = "/"
   policy = data.aws_iam_policy_document.create_eks_role.json
 }
 
 resource "aws_iam_role" "create_eks_role" {
-  name = "${var.deploy_id}-create-eks"
+  name = local.create_eks_role_name
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
