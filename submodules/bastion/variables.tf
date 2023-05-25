27be81a20023d2f6749e7b11e3e606ddc42acaa3
Miguel Harmant <miguel.harmant@dominodatalab.com>
2023-05-25 10:34:29 -0400

    PLAT-6697 Add support for af and me regions

--- submodules/bastion/variables.tf
+++ submodules/bastion/variables.tf
@@ -8,7 +8,7 @@ variable "region" {
   type        = string
   nullable    = false
   validation {
-    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-[0-9]", var.region))
+    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa|me|af)-(central|(north|south)?(east|west)?)-[0-9]", var.region))
     error_message = "The provided region must follow the format of AWS region names, e.g., us-west-2, us-gov-west-1."
   }
 }
