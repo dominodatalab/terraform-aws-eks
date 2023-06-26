5718c6fde451b82344d801e50f592530a83f0720
miguelhar <98769216+miguelhar@users.noreply.github.com>
2023-06-26 14:49:34 -0400

    Precommit to check for hardcoded aws partition (#108)

---
+++ bin/check-aws-partition.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+exec 1>&2
+
+check_aws_partition() {
+  declare -A failed_files
+
+  for file in $(git ls-files | grep -v "check-aws-partition.sh"); do
+    if grep -q "arn:aws" "${file}"; then
+      failed_files["${file}"]=1
+    fi
+  done
+
+  if [ ${#failed_files[@]} -ne 0 ]; then
+    for file in "${!failed_files[@]}"; do
+      echo "${file} contains a hardcoded AWS partition. Use arn:\${data.aws_partition.current.partition} instead of arn:aws."
+    done
+    return 1
+  fi
+
+  return 0
+
+}
+
+check_aws_partition
+exit_code=$?
+exit $exit_code
