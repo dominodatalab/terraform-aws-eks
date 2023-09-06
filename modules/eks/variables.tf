dc76173a3b6a6b6981b6a91d709b2a746ce9d564
Luis De Bello <luis.debello@dominodatalab.com>
2023-09-06 13:19:01 -0300

    Fixing condition

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -231,7 +231,7 @@ variable "privatelink" {
   })
 
   validation {
-    condition     = var.privatelink.enabled && var.privatelink.route53_hosted_zone_name != null
+    condition     = !var.privatelink.enabled || (var.privatelink.enabled && var.privatelink.route53_hosted_zone_name)
     error_message = "Route53 Hosted Zone Name cannot be null"
   }
 
