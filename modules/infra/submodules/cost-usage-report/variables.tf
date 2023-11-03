0a00c58c6a233b94b9374b2adfd1a0c53d6441c5
ddl-juan-cistaro <juan.cistaro@dominodatalab.com>
2023-11-03 12:30:40 -0300

    var back in place

--- modules/infra/submodules/cost-usage-report/variables.tf
+++ modules/infra/submodules/cost-usage-report/variables.tf
@@ -142,3 +142,12 @@ variable "network_info" {
     })
   })
 }
+
+variable "domino_cur" {
+  description = "Determines whether to provision domino cost related infrastructures, ie, long term storage"
+  type = object({
+    provision_resources = optional(bool, true)
+  })
+
+  default = {}
+}
