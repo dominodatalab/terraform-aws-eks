dd49ab7dd778b0f8729f848119ce600cb279f2dd
ddl-olsonJD <olson.dimanche@dominodatalab.com>
2023-11-05 08:29:12 -0800

    removed upload rule

--- modules/infra/submodules/storage/s3.tf
+++ modules/infra/submodules/storage/s3.tf
@@ -494,16 +494,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "costs" {
 
     status = "Enabled"
   }
-
-  rule {
-    id = "incomplete_upload"
-
-    abort_incomplete_multipart_upload {
-      days_after_initiation = 7
-    }
-
-    status = "Enabled"
-  }
 }
 
 data "aws_iam_policy_document" "costs" {
