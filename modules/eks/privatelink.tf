afdb16fbe39661096eff55800631f4133135f04c
ddl-olsonJD <olson.dimanche@dominodatalab.com>
2023-11-14 10:23:47 -0800

    Updated privatelink vars

--- modules/eks/privatelink.tf
+++ modules/eks/privatelink.tf
@@ -2,7 +2,6 @@ module "privatelink" {
   count            = var.privatelink.enabled ? 1 : 0
   source           = "./submodules/privatelink"
   deploy_id        = var.deploy_id
-  region           = var.region
   network_info     = var.network_info
   privatelink      = var.privatelink
   oidc_provider_id = aws_iam_openid_connect_provider.oidc_provider.id
