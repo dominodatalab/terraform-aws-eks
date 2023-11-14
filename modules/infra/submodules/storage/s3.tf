347c1b47ba0b9d7c2a51bad3e4036fac539d25c0
ddl-olsonJD <olson.dimanche@dominodatalab.com>
2023-11-14 08:10:13 -0800

    Updated expiration length.

--- modules/infra/submodules/storage/s3.tf
+++ modules/infra/submodules/storage/s3.tf
@@ -486,11 +486,30 @@ resource "aws_s3_bucket_lifecycle_configuration" "costs" {
   bucket = aws_s3_bucket.costs[0].id
 
   rule {
-    id = "expiration"
+    id = "AssetsExpiration"
 
     expiration {
-      days = 30
+      days = 15
     }
+
+    filter {
+       prefix = "federated/${var.deploy_id}/etl/bingen/assets/"
+    }
+
+    status = "Enabled"
+  }
+
+
+  rule {
+    id = "AllocationsExpiration"
+
+    expiration {
+      days = 15
+    }
+
+    filter {
+       prefix = "federated/${var.deploy_id}/etl/bingen/allocations/"
+    },
 
     status = "Enabled"
   }
