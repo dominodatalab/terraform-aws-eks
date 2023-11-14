79a4ba7bf3419ad3e7b6983af8a4c5cfbdd105a3
Jade Hayes <jade.hayes@dominodatalab.com>
2023-11-14 11:12:46 -0800

    * Updated submodules private link linting errors.

--- modules/eks/submodules/privatelink/variables.tf
+++ modules/eks/submodules/privatelink/variables.tf
@@ -8,16 +8,6 @@ variable "deploy_id" {
   }
 }
 
-variable "region" {
-  type        = string
-  description = "AWS region for the deployment"
-  nullable    = false
-  validation {
-    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa|me|af|il)-(central|(north|south)?(east|west)?)-[0-9]", var.region))
-    error_message = "The provided region must follow the format of AWS region names, e.g., us-west-2, us-gov-west-1."
-  }
-}
-
 variable "network_info" {
   description = <<EOF
     {
