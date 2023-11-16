6ad3c34a05f14ffc60b555c7b00499446eaf86cb
Miguel Harmant <miguel.harmant@dominodatalab.com>
2023-11-16 11:09:12 -0500

    Add eip, elb outputs

--- modules/eks/submodules/privatelink/outputs.tf
+++ modules/eks/submodules/privatelink/outputs.tf
@@ -2,5 +2,6 @@ output "info" {
   description = "Target groups..."
   value = {
     target_groups = { for k, v in aws_lb_target_group.target_groups : k => v.arn }
+    nlbs          = { arns : [for k, nlb in aws_lb.nlbs : nlb.arn] }
   }
 }
