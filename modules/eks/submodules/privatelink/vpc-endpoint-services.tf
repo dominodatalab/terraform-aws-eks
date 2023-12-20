91a2506735358a7064a6d6ca699ff3924124016c
Luis De Bello <luis.debello@dominodatalab.com>
2023-12-20 16:28:39 -0300

    [DOM-53042] Provide single target group for each service and port

--- modules/eks/submodules/privatelink/vpc-endpoint-services.tf
+++ modules/eks/submodules/privatelink/vpc-endpoint-services.tf
@@ -35,12 +35,16 @@ resource "aws_lb" "nlbs" {
 }
 
 resource "aws_lb_target_group" "target_groups" {
-  for_each = local.endpoint_services
+  for_each = { for entry in local.listeners : "${entry.service}.${entry.port}" => entry }
 
-  name     = "${var.deploy_id}-${each.key}"
+  name     = substr("${var.deploy_id}-${each.value.service}-${each.value.port}", -32, -1)
   port     = 80 # Not used but required
   protocol = "TCP"
   vpc_id   = var.network_info.vpc_id
+
+  tags = {
+    "Name" = "${var.deploy_id}-${each.value.service}-${each.value.port}"
+  }
 }
 
 resource "aws_lb_listener" "listeners" {
