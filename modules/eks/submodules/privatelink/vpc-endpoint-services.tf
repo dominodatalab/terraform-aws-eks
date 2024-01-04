42dd9899f9755cd63b309a13cd4188fda591a067
Luis De Bello <105391279+ldebello-ddl@users.noreply.github.com>
2024-01-04 17:42:59 -0300

    [DOM-53463] Fixing target group name (#194)
    * [DOM-53463] Fixing target group name
    
    * Linter
    
    * Linter
    
    * Docs
    
    * Adding version to providers
    
    * Docs
    
    * PR Feedback
    
    * linter
    
    * pr feedback
    
    * Formatting

--- modules/eks/submodules/privatelink/vpc-endpoint-services.tf
+++ modules/eks/submodules/privatelink/vpc-endpoint-services.tf
@@ -37,13 +37,14 @@ resource "aws_lb" "nlbs" {
 resource "aws_lb_target_group" "target_groups" {
   for_each = { for entry in local.listeners : "${entry.service}.${entry.port}" => entry }
 
-  name     = substr("${var.deploy_id}-${each.value.service}-${each.value.port}", -32, -1)
+  name     = "${var.deploy_id}-${substr(each.value.service, 0, 9)}-${each.value.port}"
   port     = 80 # Not used but required
   protocol = "TCP"
   vpc_id   = var.network_info.vpc_id
 
   tags = {
-    "Name" = "${var.deploy_id}-${each.value.service}-${each.value.port}"
+    "service_name" = each.value.service
+    "service_port" = each.value.port
   }
 }
 
