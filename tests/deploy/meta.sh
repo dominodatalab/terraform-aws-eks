486e8fc3c7d8df2ca13c8ef8e1935ab0a27660bd
Miguel Harmant <miguel.harmant@dominodatalab.com>
2024-01-09 16:01:30 -0500

    fix tests, hacky source

--- tests/deploy/meta.sh
+++ tests/deploy/meta.sh
@@ -9,4 +9,20 @@ PVT_KEY="${DEPLOY_DIR}/domino.pem"
 INFRA_VARS_TPL="${SH_DIR}/infra-ci.tfvars.tftpl"
 CLUSTER_VARS_TPL="${SH_DIR}/cluster-ci.tfvars.tftpl"
 
-export SH_DIR CI_DEPLOY DEPLOY_DIR PVT_KEY INFRA_VARS_TPL CLUSTER_VARS_TPL
+declare -A COMP_MODS
+COMP_MODS["infra"]="infra"
+COMP_MODS["cluster"]="eks irsa_external_dns irsa_policies"
+COMP_MODS["nodes"]="nodes"
+
+declare -A MOD_ADD
+MOD_ADD["irsa_external_dns"]="eks/submodules/irsa"
+MOD_ADD["irsa_policies"]="eks/submodules/irsa"
+
+export SH_DIR \
+  CI_DEPLOY \
+  DEPLOY_DIR \
+  PVT_KEY \
+  INFRA_VARS_TPL \
+  CLUSTER_VARS_TPL \
+  COMP_MODS \
+  MOD_ADD
