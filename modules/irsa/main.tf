15ed744aa6c7edcffca2a5816a543e6bba478916
miguelhar <98769216+miguelhar@users.noreply.github.com>
2024-02-02 12:18:28 -0500

    get eks account to flyte (#209)
    * get eks account to flyte
    
    * fix reference to account_id
    
    ---------
    
    Co-authored-by: noahjax <noah.jackson@dominodatalab.com>

--- modules/irsa/main.tf
+++ modules/irsa/main.tf
@@ -4,7 +4,7 @@ locals {
   oidc_provider_url = var.use_cluster_odc_idp ? var.eks_info.cluster.oidc.cert.url : aws_iam_openid_connect_provider.this[0].url
   oidc_provider_arn = var.use_cluster_odc_idp ? var.eks_info.cluster.oidc.arn : aws_iam_openid_connect_provider.this[0].arn
   name_prefix       = var.eks_info.cluster.specs.name
-  aws_account_id    = data.aws_caller_identity.aws_account.account_id
+  aws_account_id    = var.use_cluster_odc_idp ? var.eks_info.cluster.specs.account_id : data.aws_caller_identity.aws_account.account_id
 }
 
 resource "aws_iam_openid_connect_provider" "this" {
