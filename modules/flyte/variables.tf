ac5ec46a22c054b45b5c9d5f1307cb7e3b919019
ddl-rliu <richard.liu@dominodatalab.com>
2024-03-05 15:13:04 -0800

    KMS as default

--- modules/flyte/variables.tf
+++ modules/flyte/variables.tf
@@ -58,3 +58,26 @@ variable "serviceaccount_names" {
 
   default = {}
 }
+
+variable "kms_info" {
+  description = <<EOF
+    key_id  = KMS key id.
+    key_arn = KMS key arn.
+    enabled = KMS key is enabled
+  EOF
+  type = object({
+    key_id  = string
+    key_arn = string
+    enabled = bool
+  })
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region for the deployment"
+  nullable    = false
+  validation {
+    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa|me|af|il)-(central|(north|south)?(east|west)?)-[0-9]", var.region))
+    error_message = "The provided region must follow the format of AWS region names, e.g., us-west-2, us-gov-west-1."
+  }
+}
