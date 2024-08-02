478858b5ff29e7c26f02fd3b30fb28046d8b42f8
Miguel Harmant <miguel.harmant@dominodatalab.com>
2024-08-02 13:49:42 -0400

    Handle sg conditional better

--- modules/eks/node-group.tf
+++ modules/eks/node-group.tf
@@ -57,26 +57,26 @@ moved {
 }
 
 ### FSX
+
 locals {
   shared_storage_type = var.fsx != null ? "fsx" : "efs"
-  inbound_rules = {
-    fsx = {
-      rules = [
-        { protocol = "all", from_port = 0, to_port = 65535, description = "All traffic from EKS nodes." },
-      ]
-      security_group_id = try(var.fsx.filesystem.security_group_id)
-    }
-    efs = {
-      security_group_id = try(var.efs_security_group)
-    rules = [{ protocol = "tcp", from_port = 2049, to_port = 2049, description = "EFS access" }] }
+  inbound_rules = local.shared_storage_type == "fsx" ? {
+    rules = [
+      { protocol = "all", from_port = 0, to_port = 65535, description = "All traffic from EKS nodes." },
+    ]
+    security_group_id = var.fsx.filesystem.security_group_id
+    } : {
+    rules = [
+      { protocol = "tcp", from_port = 2049, to_port = 2049, description = "EFS access" }
+    ]
+    security_group_id = var.efs_security_group
   }
-
 }
 
 resource "aws_security_group_rule" "shared_storage" {
-  for_each = { for r in local.inbound_rules[local.shared_storage_type].rules : "${local.shared_storage_type}_${r.from_port}_${r.to_port}" => r }
+  for_each = { for r in local.inbound_rules.rules : "${local.shared_storage_type}_${r.from_port}_${r.to_port}" => r }
 
-  security_group_id        = local.inbound_rules[local.shared_storage_type].security_group_id
+  security_group_id        = local.inbound_rules.security_group_id
   source_security_group_id = aws_security_group.eks_nodes.id
   protocol                 = each.value.protocol
   from_port                = each.value.from_port
