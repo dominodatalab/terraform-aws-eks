df2e7f141f7c10b32ee3e46a78cd318747ee0985
David McWhorter <david.mcwhorter@dominodatalab.com>
2024-08-20 09:24:07 -0400

    fmt docs etc

--- modules/irsa/outputs.tf
+++ modules/irsa/outputs.tf
@@ -23,7 +23,7 @@ output "netapp_trident_operator" {
 output "external_deployments_operator" {
   description = "External deployments operator role info"
   value = var.external_deployments_operator.enabled ? {
-    irsa_role = aws_iam_role.external_deployments_operator[0].arn
+    irsa_role            = aws_iam_role.external_deployments_operator[0].arn
     service_account_name = var.external_deployments_operator.serviceaccount_name
   } : null
 }
