bcce1ec96f303192cf79c7d27b9dcf6f582be137
David McWhorter <david.mcwhorter@dominodatalab.com>
2024-09-16 11:27:55 -0400

    add comment explaining pin

--- tests/plan/terraform/versions.tf
+++ tests/plan/terraform/versions.tf
@@ -11,7 +11,8 @@ terraform {
   required_version = ">= 1.4.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
+      # pinned until https://github.com/hashicorp/terraform-provider-aws/pull/39328 is released
       version = "<5.67.0"
     }
   }
