40517a31cf2f1e2c1f1b8d16b2c73af81a61b435
Miguel Harmant <miguel.harmant@dominodatalab.com>
2024-09-17 16:15:08 -0400

    setup datasync sg

--- modules/infra/submodules/storage/datasync.tf
+++ modules/infra/submodules/storage/datasync.tf
@@ -1,3 +1,52 @@
+locals {
+  datasync_security_group_rules = toset([
+    {
+      protocol                 = "-1"
+      from_port                = 0
+      to_port                  = 0
+      description              = "Allow all traffic to DataSync"
+      type                     = "ingress"
+      source_security_group_id = aws_security_group.datasync[0].id
+    }
+  ])
+}
+
+resource "aws_security_group" "datasync" {
+  count  = var.storage.netapp.migrate_from_efs.datasync.enabled ? 1 : 0
+  name   = "${var.deploy_id}-datasync"
+  vpc_id = var.network_info.vpc_id
+
+  description = "Datasync security group"
+
+  tags = {
+    "Name" = "${var.deploy_id}-datasync"
+  }
+}
+
+resource "aws_security_group_rule" "efs_ingress" {
+  for_each = local.datasync_security_group_rules
+
+  security_group_id        = aws_security_group.efs[0].id
+  protocol                 = each.value.protocol
+  from_port                = each.value.from_port
+  to_port                  = each.value.to_port
+  type                     = each.value.type
+  description              = each.value.description
+  source_security_group_id = each.value.source_security_group_id
+}
+
+resource "aws_security_group_rule" "fsx_ingress" {
+  for_each = local.datasync_security_group_rules
+
+  security_group_id        = aws_security_group.netapp[0].id
+  protocol                 = each.value.protocol
+  from_port                = each.value.from_port
+  to_port                  = each.value.to_port
+  type                     = each.value.type
+  description              = each.value.description
+  source_security_group_id = each.value.source_security_group_id
+}
+
 data "aws_subnet" "ds" {
   count = var.storage.netapp.migrate_from_efs.datasync.enabled ? 1 : 0
   id    = var.network_info.subnets.private[0].subnet_id
@@ -8,14 +57,14 @@ resource "aws_datasync_location_efs" "this" {
   efs_file_system_arn = aws_efs_file_system.eks[0].arn
   subdirectory        = "/"
   ec2_config {
-    security_group_arns = [aws_security_group.efs[0].arn]
+    security_group_arns = [aws_security_group.datasync[0].arn]
     subnet_arn          = data.aws_subnet.ds[0].arn
   }
 }
 
 resource "aws_datasync_location_fsx_ontap_file_system" "this" {
   count                       = var.storage.netapp.migrate_from_efs.datasync.enabled ? 1 : 0
-  security_group_arns         = [aws_security_group.netapp[0].arn]
+  security_group_arns         = [aws_security_group.datasync[0].arn]
   storage_virtual_machine_arn = aws_fsx_ontap_storage_virtual_machine.eks[0].arn
 
   protocol {
