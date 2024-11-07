533ce928bd9765c22e2711871307ae6751657481
Marcos Singermann <marcos.singermann@dominodatalab.com>
2024-11-07 18:33:56 -0300

    fix

--- modules/infra/submodules/network/vpc.tf
+++ modules/infra/submodules/network/vpc.tf
@@ -59,7 +59,7 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   private_dns_enabled = true
   service_name        = "com.amazonaws.${var.region}.ecr.dkr"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = aws_subnet.pod[*].id
+  subnet_ids          = [for s in aws_subnet.pod : s.id]
 
   tags = {
     "Name" = "${var.deploy_id}-ecr-dkr"
