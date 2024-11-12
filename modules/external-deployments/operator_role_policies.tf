e7f5b30bc67858d288fbfabb376d20d73a956d4c
Tuan Nguyen <tuan.nguyen@dominodatalab.com>
2024-11-12 15:07:19 -0500

    Add role policies

--- modules/external-deployments/operator_role_policies.tf
+++ modules/external-deployments/operator_role_policies.tf
@@ -126,6 +126,8 @@ data "aws_iam_policy_document" "in_account_policies" {
       "sagemaker:DescribeModel",
       "sagemaker:InvokeEndpoint",
       "sagemaker:InvokeEndpointWithResponseStream",
+      "sagemaker:ListEndpointConfigs",
+      "sagemaker:ListModels",
       "sagemaker:UpdateEndpoint",
       "sagemaker:UpdateEndpointWeightsAndCapacities"
     ]
