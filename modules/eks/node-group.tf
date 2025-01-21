b93560ec43a3e46b1eb6ce74bdd69ae532e6a96a
Miguel Harmant <miguel.harmant@dominodatalab.com>
2025-01-21 14:01:29 -0500

    add try to conditionals

--- modules/eks/node-group.tf
+++ modules/eks/node-group.tf
@@ -63,7 +63,7 @@ moved {
 }
 
 resource "aws_security_group_rule" "efs" {
-  count                    = var.storage_info.efs != null ? 1 : 0
+  count                    = try(var.storage_info.efs, null) != null ? 1 : 0
   security_group_id        = var.storage_info.efs.security_group_id
   protocol                 = "tcp"
   from_port                = 2049
@@ -74,7 +74,7 @@ resource "aws_security_group_rule" "efs" {
 }
 
 resource "aws_security_group_rule" "netapp" {
-  count                    = var.storage_info.netapp != null ? 1 : 0
+  count                    = try(var.storage_info.netapp, null) != null ? 1 : 0
   security_group_id        = var.storage_info.netapp.filesystem.security_group_id
   protocol                 = "-1"
   from_port                = 0
