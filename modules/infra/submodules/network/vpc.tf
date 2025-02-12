670945483a5a32c3537df036e5ea89b9e2859ecb
Marcos Singermann <marcos.singermann@dominodatalab.com>
2025-02-12 16:47:13 -0300

    fmt

--- modules/infra/submodules/network/vpc.tf
+++ modules/infra/submodules/network/vpc.tf
@@ -50,10 +50,10 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint" "s3_interface" {
-  count             = local.create_vpc ? 1 : 0
-  vpc_id            = aws_vpc.this[0].id
-  service_name      = "com.amazonaws.${var.region}.s3"
-  vpc_endpoint_type = "Interface"
+  count               = local.create_vpc ? 1 : 0
+  vpc_id              = aws_vpc.this[0].id
+  service_name        = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
   subnet_ids          = [for s in aws_subnet.pod : s.id]
 
