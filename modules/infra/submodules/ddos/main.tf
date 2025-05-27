d339a6fef1355391cfb68e9a829b07071c94f911
Luis De Bello <luis.debello@dominodatalab.com>
2025-05-27 14:38:27 -0300

    var rename

--- modules/infra/submodules/ddos/main.tf
+++ modules/infra/submodules/ddos/main.tf
@@ -4,8 +4,8 @@ resource "aws_globalaccelerator_accelerator" "main_accelerator" {
   ip_address_type = "IPV4"
 
   attributes {
-    flow_logs_enabled   = var.flow_logs.enabled
-    flow_logs_s3_bucket = var.flow_logs.s3_bucket
-    flow_logs_s3_prefix = var.flow_logs.s3_prefix
+    flow_logs_enabled   = var.access_logs.enabled
+    flow_logs_s3_bucket = var.access_logs.s3_bucket
+    flow_logs_s3_prefix = var.access_logs.s3_prefix
   }
 }
