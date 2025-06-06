e9394074aeeb2b2b234d9f2145c5215284156d94
Luis De Bello <luis.debello@dominodatalab.com>
2025-06-06 16:29:32 -0300

    fixes

--- modules/eks/submodules/load-balancers/versions.tf
+++ modules/eks/submodules/load-balancers/versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.1.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
