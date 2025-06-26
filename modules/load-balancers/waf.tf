58d37199089fc4c23d2c045094e7cc149e621393
Luis De Bello <luis.debello@dominodatalab.com>
2025-06-25 23:40:03 -0300

    fixing tests

--- modules/load-balancers/waf.tf
+++ modules/load-balancers/waf.tf
@@ -204,6 +204,8 @@ resource "aws_cloudwatch_log_group" "waf_logs" {
 }
 
 resource "aws_wafv2_web_acl_logging_configuration" "application" {
+  count = var.waf.enabled ? 1 : 0
+
   log_destination_configs = [aws_cloudwatch_log_group.waf_logs.arn]
   resource_arn            = aws_wafv2_web_acl.waf[0].arn
 
