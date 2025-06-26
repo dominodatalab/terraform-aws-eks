ecaaf8a8fd64f4ffcef3be84d389a6477ddfd431
Luis De Bello <luis.debello@dominodatalab.com>
2025-06-26 10:24:14 -0300

    region

--- modules/load-balancers/variables.tf
+++ modules/load-balancers/variables.tf
@@ -198,3 +198,13 @@ variable "network_info" {
     })
   })
 }
+
+variable "region" {
+  type        = string
+  description = "AWS region for the deployment"
+  nullable    = false
+  validation {
+    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa|me|af|il)-(central|(north|south)?(east|west)?)-[0-9]", var.region))
+    error_message = "The provided region must follow the format of AWS region names, e.g., us-west-2, us-gov-west-1."
+  }
+}
