3fa5c92d8c7ed4d245bdb6edff3c791e5ac9df5d
Luis De Bello <luis.debello@dominodatalab.com>
2025-09-10 19:45:53 -0300

    Fix policy

--- modules/infra/kms.tf
+++ modules/infra/kms.tf
@@ -114,7 +114,7 @@ data "aws_iam_policy_document" "provided_key_policy_document" {
   count = local.provided_key
   source_policy_documents = [
     templatefile("${path.module}/provided_key_policy_document.json", {
-      partition = data.aws_partition.current.partition
+      kms_key = data.aws_kms_key.key[0]
     })
   ]
 }
