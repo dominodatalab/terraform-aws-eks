db4943a0da89f41f37347b389624adbf0e11e6c7
Miguel Harmant <98769216+miguelhar@users.noreply.github.com>
2026-05-21 12:41:21 -0400

    PLAT-9916: Relax aws_region validation regex to accept non-public AWS regions (us-iso*, us-isob*, us-isof*) so Terraform stops rejecting valid DoD/Navy region names ahead of the API call. Aligns with the relaxed regex shipped in domino_config v1.16-rc4 (PLAT-9913). (#440)

--- modules/infra/submodules/cost-usage-report/variables.tf
+++ modules/infra/submodules/cost-usage-report/variables.tf
@@ -13,8 +13,8 @@ variable "region" {
   description = "AWS region for the deployment"
   nullable    = false
   validation {
-    condition     = can(regex("(us(-gov)?|ap|ca|cn|eu|sa|me|af|il)-(central|(north|south)?(east|west)?)-[0-9]", var.region))
-    error_message = "The provided region must follow the format of AWS region names, e.g., us-west-2, us-gov-west-1."
+    condition     = can(regex("^[a-z]{2,}(-[a-z0-9]+)*-\\w+-\\d+$", var.region))
+    error_message = "The provided region must follow the format of AWS region names, e.g., us-west-2, us-gov-west-1, us-iso-east-1."
   }
 }
 
