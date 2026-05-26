6c180f9d5b22818467fe28ee5324cd2df4a0baf1
Miguel Harmant <miguel.harmant@dominodatalab.com>
2026-05-26 11:42:13 -0400

    DOM-76079: drop unused tests/plan/dns-delegation fixture

--- tests/plan/dns-delegation/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "record_fqdn" {
-  description = "FQDN of the NS delegation record."
-  value       = module.dns_delegation.record_fqdn
-}
-
-output "record_name" {
-  description = "Name of the NS delegation record."
-  value       = module.dns_delegation.record_name
-}
